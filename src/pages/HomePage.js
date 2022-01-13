251326f6969aa2fd2d6d12e0dc676a54f4685da9
Ulisses Lopes <ulissesdvl@yahoo.com.br>
2022-01-13 18:19:36 -0300

    Adding infinite scroll

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,61 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getSchools } from "../api/schools";
+import useInfiniteScroll from 'react-infinite-scroll-hook';
+
+const initialStatus = {
+  loading: false,
+  hasNextPage: true,
+  page: 1,
+  error: false,
+  items: [],
+  hasStarted: false,
+}
 
 const HomePage = () => {
 
-  const [schools, setSchools] = useState([])
+  const [loadingStatus, setLoadingStatus] = useState(initialStatus)
 
-  console.log(schools);
+  console.log(loadingStatus);
 
-  const loadSchools = async () => {
+  const loadSchools = async (currentItems=loadingStatus.items, page=loadingStatus.page) => {
+    console.log("*** loadSchools ***");
+    if(loadingStatus.loading) return;
+    setLoadingStatus({...loadingStatus, loading: true, items: currentItems})
     try {
-      const result = await getSchools()
-      setSchools([...schools, ...result.data.docs])
+      console.log("*** starting new load ***");
+      const result = await getSchools(page)
+      setLoadingStatus({
+        ...loadingStatus,
+        loading: false,
+        error: false,
+        hasNextPage: result.data.hasNextPage,
+        page: page + 1,
+        items: [...currentItems, ...result.data.docs],
+        hasStarted: true,
+      })
     } catch(error) {
       console.warn(error);
+      setLoadingStatus({...loadingStatus, loading: false, error: true})
     }
   }
+
+  const onSearch = async () => {
+    setLoadingStatus({...initialStatus})
+    loadSchools([], 1);
+  }
+
+  const [sentryRef] = useInfiniteScroll({
+    loading: loadingStatus.loading,
+    hasNextPage: loadingStatus.hasNextPage,
+    onLoadMore: loadSchools,
+    // When there is an error, we stop infinite loading.
+    // It can be reactivated by setting "error" state as undefined.
+    disabled: !!loadingStatus.error,
+    // `rootMargin` is passed to `IntersectionObserver`.
+    // We can use it to trigger 'onLoadMore' when the sentry comes near to become
+    // visible, instead of becoming fully visible on the screen.
+    //rootMargin: '0px 0px 400px 0px',
+  });
 
   return (
     <main>
@@ -26,14 +67,17 @@ const HomePage = () => {
         Nós queremos ajudar você e outros estudantes
       </p>
       </header>
-      <button onClick={loadSchools}>Buscar</button>
+      <button onClick={onSearch}>Buscar</button>
       <ul>
-        {schools.map(school => (
+        {loadingStatus.items.map(school => (
           <li key={`school-${school._id}`}>
             <h2>{school.name}</h2>
           </li>
         ))}
       </ul>
+      {(loadingStatus.loading || loadingStatus.hasStarted) && (
+        <p ref={sentryRef}>Carregando...</p>
+      )}
     </main>
   )
 
